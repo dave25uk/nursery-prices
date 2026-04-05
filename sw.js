7ca2eb43160d656a8221042922fb4125cab0687e
dave25uk <dave.brown04@live.com>
2026-04-05 15:04:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'old-forge-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve the app from cache when offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
