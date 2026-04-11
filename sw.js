572b9ba95bd43c25240a8f802a6a071b3b871081
dave25uk <dave.brown04@live.com>
2026-04-11 15:47:38 +0100

    Revert "bug fix"
    This reverts commit e3541680961d4b0c7da25f27aa6a858e512d7b65.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'forge-nursery-v13.1';
+const CACHE_NAME = 'forge-nursery-v13';
 const ASSETS = [
   './',
   './index.html',
@@ -9,30 +9,29 @@ const ASSETS = [
   './icon-192.png'
 ];
 
-// Install: Cache everything
+// Install: Save files to cache
 self.addEventListener('install', (e) => {
-  self.skipWaiting(); // Force the new service worker to become active immediately
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate: Clean up old caches
+// Fetch: Serve from cache if offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// Activate: Clean up old versions
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
-    })
-  );
-});
-
-// Fetch: Try network, then cache
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
     })
   );
 });
